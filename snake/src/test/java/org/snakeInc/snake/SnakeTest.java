8af0359480b2ac997b1285d948d30b9d653711ec
dmassif <dmassif@centrale-marseille.fr>
2025-11-17 11:10:02 +0100

    Make direction enum

--- snake/src/test/java/org/snakeInc/snake/SnakeTest.java
+++ snake/src/test/java/org/snakeInc/snake/SnakeTest.java
@@ -2,8 +2,10 @@ package org.snakeInc.snake;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.snakeinc.snake.GameParams;
 import org.snakeinc.snake.exception.OutOfPlayException;
 import org.snakeinc.snake.exception.SelfCollisionException;
+import org.snakeinc.snake.model.Direction;
 import org.snakeinc.snake.model.Game;
 
 public class SnakeTest {
@@ -13,15 +15,38 @@ public class SnakeTest {
     @Test
     public void snakeEatApplesAfterMove_ReturnsCorrectBodySize() throws OutOfPlayException, SelfCollisionException {
         game.getBasket().addApple(game.getGrid().getTile(5, 4));
-        game.getSnake().move('U');
+        game.getSnake().move(Direction.UP);
         Assertions.assertEquals(2, game.getSnake().getSize());
     }
 
     @Test
     void snakeMovesUp_ReturnCorrectHead() throws OutOfPlayException, SelfCollisionException {
-        game.getSnake().move('U');
+        game.getSnake().move(Direction.UP);
         Assertions.assertEquals(5, game.getSnake().getHead().getX());
         Assertions.assertEquals(4, game.getSnake().getHead().getY());
     }
 
+    @Test
+    void snakeOutOfPlay_ThrowsOutOfPlayException() {
+        Assertions.assertThrows(OutOfPlayException.class, () -> {
+            for (int i = 0; i < GameParams.SNAKE_DEFAULT_Y + 1; i++) {
+                game.iterate(Direction.UP);
+            }
+        }, "Expected to throw OutOfPlayException");
+    }
+
+    @Test
+    void snakeSelfCollision_ThrowsSelfCollisionException() throws OutOfPlayException, SelfCollisionException {
+        game.getBasket().addApple(game.getGrid().getTile(5, 4));
+        game.getBasket().addApple(game.getGrid().getTile(5, 3));
+        game.getBasket().addApple(game.getGrid().getTile(5, 2));
+        game.iterate(Direction.UP);
+        game.iterate(Direction.UP);
+        game.iterate(Direction.UP);
+        game.iterate(Direction.LEFT);
+        game.iterate(Direction.DOWN);
+        Assertions.assertThrows(SelfCollisionException.class, () -> {
+            game.iterate(Direction.RIGHT);
+        }, "Expected to throw SelfCollisionException");
+    }
 }
